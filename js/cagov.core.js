7e497c2b3ac71a153a82211d105b04f212b04fbe
Danny Guzman <guzmanjd86@gmail.com>
2018-04-30 12:10:17 -0700

    Fixed issue with $ not a function

--- js/cagov.core.js
+++ js/cagov.core.js
@@ -5,7 +5,7 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
-
+$ = jQuery.noConflict();
 
 +function ($) {
   'use strict';
