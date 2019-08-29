cc55f19fd0640a5872cd75195d2bce4fe14e8d6a
konstantin.koryaka <konstantin.koryaka@state.ca.gov>
2019-08-29 10:09:22 -0700

    - Updated Respond.min.js and html5shiv.min.js to the latest versions - Updatated template version in files to the v5.5.7

--- js/cagov.core.js
+++ js/cagov.core.js
@@ -1,5 +1,5 @@
 /**
- * CA State Template v5.5 -  @version v5.5.6 -  8/29/2019 
+ * CA State Template v5.5 -  @version v5.5.7 -  8/29/2019 
   STYLES COMPILED FROM SOURCE (source/js) DO NOT MODIFY */
 /*! modernizr (Custom Build) | MIT *
  * https://modernizr.com/download/?-flexbox-setclasses !*/
