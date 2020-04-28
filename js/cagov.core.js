74c9d296937d0eb4a870ceddd94ffe763ef6d1c2
Konstantin Koryaka <konstanitn.koryaka@state.ca.gov>
2020-04-28 14:51:22 -0700

    Updated files to v6.0.2

--- js/cagov.core.js
+++ js/cagov.core.js
@@ -1,5 +1,5 @@
 /**
- * CA State Template v6 -  @version v6.0.1 -  4/27/2020 
+ * CA State Template v6 -  @version v6.0.2 -  4/27/2020 
   STYLES COMPILED FROM SOURCE (source/js) DO NOT MODIFY */
 /*! modernizr (Custom Build) | MIT *
  * https://modernizr.com/download/?-flexbox-setclasses !*/
