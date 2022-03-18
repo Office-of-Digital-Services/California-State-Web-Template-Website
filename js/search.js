b0e26fd9a53ae17ea136182e273a50e3b569f788
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2022-03-18 15:04:47 -0700

    Updated GA code in GA analytics files #189

--- js/search.js
+++ js/search.js
@@ -4,7 +4,7 @@
 
 // Google Analytics
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-3419582-2']); // Step 4: your google analytics profile code, either from your own google account, or contact eServices to have one set up for you
+_gaq.push(['_setAccount', 'UA-20973992-30']); // Step 4: your google analytics profile code, either from your own google account, or contact eServices to have one set up for you
 _gaq.push(['_gat._anonymizeIp']);
 _gaq.push(['_setDomainName', '.ca.gov']);
 _gaq.push(['_trackPageview']);
@@ -14,11 +14,11 @@ _gaq.push(['b._setDomainName', '.ca.gov']);
 _gaq.push(['b._trackPageview']);
 
 (function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' === document.location.protocol ? 'https://ssl' :
-      'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = ('https:' === document.location.protocol ? 'https://ssl' :
+    'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
 })();
