8f9eedd5310c937777f9d63bc663992ece467385
Carter Medlin <carterm@users.noreply.github.com>
2023-04-03 10:47:10 -0700

    src/js/cagov

--- src/js/cagov/return-top.js
+++ src/js/cagov/return-top.js
@@ -1,16 +1,12 @@
 const returnTop = document.querySelector(".return-top");
 
-
 // Add on-click event
 returnTop.addEventListener("click", goToTopFunction);
 
 function goToTopFunction() {
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
-
 
 // If an user scrolls down the page for more than 400px activate back to top button
 // othervise keep it invisible
@@ -18,55 +14,55 @@ var timer;
 
 var lastScrollTop = 0;
 
-window.addEventListener("scroll", function () {
-	var returnTopButton = document.querySelector(".return-top");
-	var st = window.pageYOffset || document.documentElement.scrollTop;
-	if (st > lastScrollTop) {
-		// downscroll code
-		returnTopButton.classList.remove("is-visible");
+window.addEventListener(
+  "scroll",
+  function () {
+    var returnTopButton = document.querySelector(".return-top");
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      // downscroll code
+      returnTopButton.classList.remove("is-visible");
+    } else {
+      // upscroll code
+      if (
+        document.body.scrollTop >= 400 ||
+        document.documentElement.scrollTop >= 400
+      ) {
+        if (timer != "undefined") {
+          clearTimeout(timer);
+        }
+        returnTopButton.classList.add("is-visible");
 
-	} else {
-		// upscroll code
-		if (document.body.scrollTop >= 400 || document.documentElement.scrollTop >= 400) {
-			if (timer != "undefined") {
-				clearTimeout(timer);
-			}
-			returnTopButton.classList.add("is-visible");
-
-			timer = setTimeout(function () {
-
-				returnTopButton.classList.remove("is-visible");
-
-			}, 2000) //Back to top removes itself after 2 sec of inactivity
-		}
-		// bottom of the page 
-
-		else {
-			returnTopButton.classList.remove("is-visible");
-		}
-	}
-	lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
-
+        timer = setTimeout(function () {
+          returnTopButton.classList.remove("is-visible");
+        }, 2000); //Back to top removes itself after 2 sec of inactivity
+      }
+      // bottom of the page
+      else {
+        returnTopButton.classList.remove("is-visible");
+      }
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  },
+  false
+);
 
 // Hittin' rock bottom
 window.onscroll = function (ev) {
-	var returnTopButton = document.querySelector(".return-top");
-	if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-		returnTopButton.classList.add("is-visible");
-	}
+  var returnTopButton = document.querySelector(".return-top");
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    returnTopButton.classList.add("is-visible");
+  }
 };
-
 
 // Back to top link in the global footer
 const backToTop = document.querySelector("a[href='#skip-to-content']");
 if (backToTop) {
-	backToTop.addEventListener("click", backToTopFunction);
+  backToTop.addEventListener("click", backToTopFunction);
 }
 
-
 function backToTopFunction(event) {
-	event.preventDefault();
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  event.preventDefault();
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
