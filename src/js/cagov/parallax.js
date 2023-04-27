8f4fc8fb7ce0f4d1c152b3d9f50b76b6033693ba
jon-grant-state-ca-gov <jon.grant@state.ca.gov>
2023-04-27 14:59:45 -0700

    Add ts-check and check instanceof HTMLElement

--- src/js/cagov/parallax.js
+++ src/js/cagov/parallax.js
@@ -1,14 +1,18 @@
+//@ts-check
+
 const parallax = document.querySelector(".parallax");
 
-window.addEventListener("scroll", () => {
-  const scrolledHeight = window.pageYOffset,
-    limit = parallax.offsetTop + parallax.offsetHeight;
+if (parallax instanceof HTMLElement) {
+  window.addEventListener("scroll", () => {
+    const scrolledHeight = window.pageYOffset,
+      limit = parallax.offsetTop + parallax.offsetHeight;
 
-  if (scrolledHeight > parallax.offsetTop && scrolledHeight <= limit) {
-    parallax.style.backgroundPositionY = `${
-      (scrolledHeight - parallax.offsetTop) / 1.5
-    }px`;
-  } else {
-    parallax.style.backgroundPositionY = "0";
-  }
-});
+    if (scrolledHeight > parallax.offsetTop && scrolledHeight <= limit) {
+      parallax.style.backgroundPositionY = `${
+        (scrolledHeight - parallax.offsetTop) / 1.5
+      }px`;
+    } else {
+      parallax.style.backgroundPositionY = "0";
+    }
+  });
+}
