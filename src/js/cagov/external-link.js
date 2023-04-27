69d91e684d358b8f7a109a328d7474963a4bc5a1
jon-grant-state-ca-gov <jon.grant@state.ca.gov>
2023-04-27 15:24:14 -0700

    Linting for external-link.js

--- src/js/cagov/external-link.js
+++ src/js/cagov/external-link.js
@@ -1,8 +1,13 @@
+//@ts-check
+
 /* EXTERNAL LINK ICON */
 function linkAnnotator() {
   const ext = '<span class="external-link-icon" aria-hidden="true"></span>';
 
   // Check if link is external function
+  /**
+   * @param {HTMLAnchorElement} linkElement
+   */
   function linkIsExternal(linkElement) {
     return window.location.host.indexOf(linkElement.host) > -1;
   }
@@ -17,17 +22,19 @@ function linkAnnotator() {
     `main a${cssExceptions}, .agency-footer a${cssExceptions}, .site-footer a${cssExceptions}, footer a${cssExceptions}`
   );
   externalLink.forEach(element => {
-    const anchorLink = element.href.indexOf("#") === 0;
-    const localHost = element.href.indexOf("localhost") > -1;
-    const localEmail = element.href.indexOf("@") > -1;
-    const linkElement = element;
-    if (
-      linkIsExternal(linkElement) === false &&
-      !anchorLink &&
-      !localEmail &&
-      !localHost
-    ) {
-      linkElement.innerHTML += ext; // += concatenates to external links
+    if (element instanceof HTMLAnchorElement) {
+      const anchorLink = element.href.indexOf("#") === 0;
+      const localHost = element.href.indexOf("localhost") > -1;
+      const localEmail = element.href.indexOf("@") > -1;
+      const linkElement = element;
+      if (
+        linkIsExternal(linkElement) === false &&
+        !anchorLink &&
+        !localEmail &&
+        !localHost
+      ) {
+        linkElement.innerHTML += ext; // += concatenates to external links
+      }
     }
   });
 }
