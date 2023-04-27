1613df7d19e76f323a561ffd97d913850d7fdf34
jon-grant-state-ca-gov <jon.grant@state.ca.gov>
2023-04-27 16:21:43 -0700

    Update with JSDoc type decorator

--- src/js/cagov/external-link.js
+++ src/js/cagov/external-link.js
@@ -14,27 +14,26 @@ function linkAnnotator() {
 
   // Add any exceptions to not render here
   const cssExceptions = `:not(
- code *
-)`;
+  code *
+ )`;
 
   // Looping thru all links inside of the main content body, agency footer and statewide footer
+  /** @type {NodeListOf<HTMLAnchorElement>} */
   const externalLink = document.querySelectorAll(
     `main a${cssExceptions}, .agency-footer a${cssExceptions}, .site-footer a${cssExceptions}, footer a${cssExceptions}`
   );
   externalLink.forEach(element => {
-    if (element instanceof HTMLAnchorElement) {
-      const anchorLink = element.href.indexOf("#") === 0;
-      const localHost = element.href.indexOf("localhost") > -1;
-      const localEmail = element.href.indexOf("@") > -1;
-      const linkElement = element;
-      if (
-        linkIsExternal(linkElement) === false &&
-        !anchorLink &&
-        !localEmail &&
-        !localHost
-      ) {
-        linkElement.innerHTML += ext; // += concatenates to external links
-      }
+    const anchorLink = element.href.indexOf("#") === 0;
+    const localHost = element.href.indexOf("localhost") > -1;
+    const localEmail = element.href.indexOf("@") > -1;
+    const linkElement = element;
+    if (
+      linkIsExternal(linkElement) === false &&
+      !anchorLink &&
+      !localEmail &&
+      !localHost
+    ) {
+      linkElement.innerHTML += ext; // += concatenates to external links
     }
   });
 }
