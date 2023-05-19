36e93231175bd82df19e7d8675995d8814d643c3
kkoryaka <konstantin.koryaka@state.ca.gov>
2023-05-19 08:09:37 -0700

    Added mobile side navigation functionality and styles #853

--- src/js/cagov/side-navigation.js
+++ src/js/cagov/side-navigation.js
@@ -1,92 +1,118 @@
 //@ts-check
+
 /* -----------------------------------------
-   SIDE NAVIGATION - /source/js/cagov/side-navigation.js
+ SIDE NAVIGATION - /source/js/cagov/side-navigation.js
 ----------------------------------------- */
 
 (() => {
-  // Remember scrolling position
   const siteHeader = document.querySelector("header");
   const sidenavigation = document.querySelector(".side-navigation");
-  const mainContentSideNavCont = document.querySelector("main .row .col-md-3");
+  let allSidenavLinks = sidenavigation?.querySelectorAll(".side-navigation a");
+  const mainContentSideNavCont = sidenavigation?.closest("div");
   sidenavigation?.setAttribute("id", "side-navigation");
   const topposition = localStorage.getItem("sidebar-scroll");
   const mobileCntls = document.querySelector(".global-header .mobile-controls");
-  let mobileControlsDisplay = getComputedStyle(mobileCntls).display;
-  // Side nav height vs viewport
+  let mobileControlsDisplay = getComputedStyle(mobileCntls).display; // Side nav height vs viewport
   const siteHeaderHeight = siteHeader ? siteHeader.clientHeight : 0;
-
   const mobileView$3 = () =>
     getComputedStyle(document.querySelector(".global-header .mobile-controls"))[
       "display"
     ] !== "none";
-
   let timeout = 0;
   const delay = 250; // delay between calls
   let mobileSideNavDiv;
   let mobileSideNavCont;
+  let sidenavToggleBtn;
 
   function createMobileSideNavButton() {
     // get first side nav element
-    const sidenavTItle = document.querySelector(".side-navigation li a");
-    // get text for the button for first side nav element
-    let btnText = sidenavTItle?.innerHTML;
+    /** @type {HTMLAnchorElement} */
+    const sidenavTItle = document.querySelector(".side-navigation a, .sidenav");
 
-    // removing the sr-only span and it's content
-    btnText = btnText?.replace(/<\/?span[^>].*>/g, "");
-    // removing unessesary new lines
-    btnText = btnText?.replace(/(\r\n|\n|\r)/gm, "");
-    // removing unessesary extra spaces
-    btnText = btnText?.replace(/\s+/g, " ").trim();
-
-    // create button container
-    const sidenavMobile = document.createElement("div");
-    sidenavMobile.setAttribute("class", "sidenav-mobile-btn");
-    const sidenavMobileCont = document.createElement("div");
-    sidenavMobileCont.setAttribute("class", "container");
-    sidenavMobile.append(sidenavMobileCont);
-    // create button
-    const sidenavToggleBtn = document.createElement("button");
-    sidenavToggleBtn.setAttribute("class", "sidenav-toggle");
-    sidenavToggleBtn.setAttribute("aria-expanded", "false");
-    sidenavToggleBtn.setAttribute("aria-controls", "side-navigation");
-    sidenavToggleBtn.innerText = btnText;
-    // append button into the header
-    sidenavMobileCont.append(sidenavToggleBtn);
-    siteHeader.append(sidenavMobile);
-    // add click event
-    sidenavToggleBtn.addEventListener("click", toggleSideNav);
+    if (sidenavTItle) {
+      // get text for the button for first side nav element
+      let btnText = sidenavTItle.innerText;
+      const btnTextSpan = sidenavTItle.querySelector("span")?.innerText; // removing the sr-only span and it's content
+      btnText = btnText.replace(btnTextSpan, "").trim();
+      // create button container
+      const sidenavMobile = document.createElement("aside");
+      sidenavMobile.setAttribute("class", "sidenav-mobile-btn");
+      const sidenavMobileCont = document.createElement("div");
+      sidenavMobileCont.setAttribute("class", "container");
+      sidenavMobile.append(sidenavMobileCont);
+      // create button
+      sidenavToggleBtn = document.createElement("button");
+      sidenavToggleBtn.setAttribute("class", "sidenav-toggle");
+      sidenavToggleBtn.setAttribute("aria-expanded", "false");
+      sidenavToggleBtn.setAttribute("aria-controls", "side-navigation");
+      sidenavToggleBtn.innerText = btnText;
+      // create icon
+      const arrowIcon = document.createElement("span");
+      arrowIcon.setAttribute("aria-hidden", "true");
+      arrowIcon.setAttribute("class", "ca-gov-icon-caret-down");
+      sidenavToggleBtn.append(arrowIcon);
+      // append button into the header
+      sidenavMobileCont.append(sidenavToggleBtn);
+      siteHeader.after(sidenavMobile);
+      // add click event
+      sidenavToggleBtn.addEventListener("click", toggleSideNav);
+    }
   }
 
   function createmobileSideNavDiv() {
-    mobileSideNavDiv = document.createElement("div");
+    mobileSideNavDiv = document.createElement("aside");
     mobileSideNavDiv.setAttribute("class", "mobile-sidenav");
     mobileSideNavCont = document.createElement("div");
     mobileSideNavCont.setAttribute("class", "container");
     mobileSideNavDiv.append(mobileSideNavCont);
-    siteHeader.append(mobileSideNavDiv);
+    siteHeader.after(mobileSideNavDiv);
   }
 
-  // Functions0
+  // MOBILE Side nav
   function moveSideNavToHeader() {
     mobileSideNavCont?.append(sidenavigation);
+    sidenavigation.setAttribute("aria-hidden", "true");
+    allSidenavLinks?.forEach(el => {
+      el.setAttribute("tabindex", "-1");
+    });
   }
 
+  // DESKTOP Side nav
   function moveSideNavToMainContent() {
     mainContentSideNavCont?.append(sidenavigation);
+    sidenavigation.removeAttribute("aria-hidden");
+    allSidenavLinks?.forEach(el => {
+      el.removeAttribute("tabindex");
+    });
   }
 
+  // Mobile Side Nav Button click function
   function toggleSideNav() {
-    console.log("click");
+    mobileSideNavDiv.classList.toggle("visible");
+    // Open
+    if (mobileSideNavDiv.classList.contains("visible")) {
+      sidenavigation.removeAttribute("aria-hidden");
+      sidenavToggleBtn.setAttribute("aria-expanded", "true");
+      allSidenavLinks?.forEach(el => {
+        el.removeAttribute("tabindex");
+      });
+
+      // Closed
+    } else {
+      sidenavToggleBtn.setAttribute("aria-expanded", "false");
+      sidenavigation.setAttribute("aria-hidden", "true");
+      allSidenavLinks?.forEach(el => {
+        el.setAttribute("tabindex", "-1");
+      });
+    }
   }
 
-  // Set active class on nav-heading links:
+  // Set active class on nav-heading links
   function addActiveClass() {
     /** @type {NodeListOf<HTMLAnchorElement>} */
     const active_link = document.querySelectorAll("a.nav-heading"),
       len = active_link.length,
-      full_path = location.href.split("#")[0]; //Ignore hashes?
-
-    // Loop through each link.
+      full_path = location.href.split("#")[0]; //Ignore hashes? // Loop through each link.
     for (let i = 0; i < len; i++)
       if (active_link[i].href.split("#")[0] == full_path)
         active_link[i].className += " active";
@@ -101,8 +127,7 @@
         viewportMinusHeader <=
         document.querySelector(".side-navigation").clientHeight
       ) {
-        sidenavigation.classList.add("overflow-auto");
-        // sidenavigation.setAttribute("style", "max-height:" + viewportMinusHeader + "px")
+        sidenavigation.classList.add("overflow-auto"); // sidenavigation.setAttribute("style", "max-height:" + viewportMinusHeader + "px")
       } else {
         sidenavigation.classList.remove("overflow-auto");
         sidenavigation.removeAttribute("style");
@@ -117,9 +142,7 @@
     } else {
       sidenavigation.classList.remove("overflow-auto");
       sidenavigation.removeAttribute("style");
-    }
-
-    // Remebemering scrolling position
+    } // Remebemering scrolling position
     if (topposition !== null) {
       sidenavigation.scrollTop = parseInt(topposition, 10);
     }
@@ -134,24 +157,20 @@
   if (sidenavigation) {
     // ONLOAD
     addActiveClass();
-    createMobileSideNavButton();
     createmobileSideNavDiv();
+    createMobileSideNavButton();
+
     if (mobileControlsDisplay == "block") {
       moveSideNavToHeader();
     }
-    //  on resize
+    // on resize
     window.addEventListener("resize", () => {
-      mobileControlsDisplay = getComputedStyle(mobileCntls).display;
+      mobileControlsDisplay = getComputedStyle(mobileCntls).display; // clear the timeout
 
-      // clear the timeout
-      window.clearTimeout(timeout);
-      // start timing for event "completion"
-      timeout = window.setTimeout(sidenavOverflow, delay);
-
-      // if mobile
+      window.clearTimeout(timeout); // start timing for event "completion"
+      timeout = window.setTimeout(sidenavOverflow, delay); // if mobile
       if (mobileControlsDisplay == "block") {
-        moveSideNavToHeader();
-        // if desctop
+        moveSideNavToHeader(); // if desctop
       } else {
         moveSideNavToMainContent();
       }
